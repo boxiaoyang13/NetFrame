6a055bef618bb5373cac1d851d84487fd1a37aaf
system <system@qq.com>
2016-12-11 10:08:56 +0800

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
+
+typedef set<int, less<int>> intset;
 
 int main(int argc, char **argv)
 {
     cout << "hello world!" << endl;
-    
+
+    cout << "123"
     return 0;
 }
